327d9f4eba3aba7320dd1ed9203a8445f69c45a3
MtDeity <o8olo853q43@gmail.com>
2020-02-07 12:27:45 +0900

    Bottome Navigation View失敗

--- app/src/main/java/com/example/traininglog/HomeActivity.kt
+++ app/src/main/java/com/example/traininglog/HomeActivity.kt
@@ -2,20 +2,11 @@ package com.example.traininglog
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_home.bottomBar
-import com.iammert.library.readablebottombar.ReadableBottomBar
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        bottomBar.setOnItemSelectListener( object :ReadableBottomBar.ItemSelectListener{
-            override fun onItemSelected(index: Int) {
-            }
-        })
-
-
     }
 }
