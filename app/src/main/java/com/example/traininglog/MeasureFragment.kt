df3e2c07411e4a236c169a02e2cd3cc4d02538b8
MtDeity <o8olo853q43@gmail.com>
2020-02-18 14:25:31 +0900

    日付入力失敗

--- app/src/main/java/com/example/traininglog/MeasureFragment.kt
+++ app/src/main/java/com/example/traininglog/MeasureFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_measure.*
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class MeasureFragment : Fragment() {
@@ -19,7 +20,6 @@ class MeasureFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_measure, container, false)
     }
 
@@ -32,51 +32,22 @@ class MeasureFragment : Fragment() {
 
 
         etDate.setOnClickListener {
+            val dialogFragment = DatePickDialogFragment()
+            dialogFragment.show(fragmentManager!!, "")
         }
-
     }
 }
 
-//class DatePickDialogFragmen : DialogFragment(), DatePickerDialog.OnDateSetListener {
-//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-//    }
-//
-//    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-//    }
-//
-//
-////    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-////        val c: Calendar = Calendar.getInstance()
-////        val year: Int = c.get(Calendar.YEAR)
-////        val month: Int = c.get(Calendar.MONTH)
-////        val day: Int = c.get(Calendar.DAY_OF_MONTH)
-////        return DatePickerDialog(
-////            activity!!,
-////            activity as MainActivity?, year, month, day
-////        )
-////    }
-////
-////    override fun onDateSet(
-////        view: DatePicker?, year: Int,
-////        monthOfYear: Int, dayOfMonth: Int
-////    ) {
-////    }
-////
-////
-////    @Override
-////    @NonNull
-////    public Dialog onCreateDialog(Bundle savedInstanceState) {
-////        final Calendar c = Calendar.getInstance();
-////        int year = c.get(Calendar.YEAR);
-////        int month = c.get(Calendar.MONTH);
-////        int day = c.get(Calendar.DAY_OF_MONTH);
-////
-////        return new DatePickerDialog(getActivity(),
-////        (MainActivity)getActivity(),  year, month, day);
-////    }
-////
-////    @Override
-////    public void onDateSet(android.widget.DatePicker view, int year,
-////    int monthOfYear, int dayOfMonth) {
-////    }
-//}
+class DatePickDialogFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c: Calendar = Calendar.getInstance()
+        val year: Int = c.get(Calendar.YEAR)
+        val month: Int = c.get(Calendar.MONTH)
+        val day: Int = c.get(Calendar.DAY_OF_MONTH)
+        return DatePickerDialog(context!!, this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+    }
+}
